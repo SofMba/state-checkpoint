b3d297180505b858f3cb919bd0f052f73e7e94bd
SofMba <mbareksofiene04@gmail.com>
2020-08-07 16:00:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person:{
+      name: "Sofiene mbarek",
+    bio: " 19 years old",
+    prfession: "frond-end software devlopper"
+    },
+     maried: true,
+     time : 0
+  };
+
+
+  handleshow = () => {
+    this.setState({maried: false})
+  }
+  handleshowTwo = () => {
+    this.setState({maried: true})
+  
+  }
+  componentDidMount () {
+    setInterval(() => {
+      this.setState({time: this.state.time +1})
+    }, 1000);
+
+  }
+
+  render() {
+    return (
+      <div>
+      {this.state.maried ?
+      <div>
+      <h1>welcome </h1>
+        <h1>The name : {this.state.person.name}</h1>
+        <h1>The profession : {this.state.person.prfession}</h1>
+        <h1>The age : {this.state.person.bio}</h1>
+        <button type="button" onClick={this.handleshow}>click me !!</button>
+      </div>
+      :
+      <div>
+      <h1>welcome </h1>
+      <button type="button" onClick={this.handleshowTwo}>click me !!</button>
+      </div>
+      }
+      <h1>{this.state.time}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
